4e802237feee8fcf9e01406d0006dd21c5e423ca
kolhjuli <kolhjuli@kkg-do.eu>
2022-09-06 11:27:27 +0200

    e1-3

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,19 @@
 public class Employee {
+    private String name,firstName,birthdate, specialties,department = "A38";
+    private char gender;
+    private int age,workYears = 0;
+    private double weight,salary = 1500;
+
+    //public Employee(String name,String firstName,String birthdate,String specialties,String department,char gender,int age,int workYears,double weight,double salary){
+
+    //}
+    public Employee(){
+
+    }
+
+
+
+
+
+
 }
--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,5 +1,7 @@
 public class MainProgram {
     public static void main(String[] args) {
-
+        Employee e1 = new Employee();
+        Employee e2 = new Employee();
+        Employee e3 = new Employee();
     }
 }
